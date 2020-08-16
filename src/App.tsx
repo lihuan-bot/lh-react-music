7ab7e43985ed3a95de508f36b9b1ffbe0c544c64
lihuan <1968763315@qq.com>
2020-08-16 12:54:36 +0800

    初始化样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Button } from 'antd'
-import './App.less';
+import React from "react";
+import { Button } from "antd";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button type="primary">aa</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
+      <Button type="dashed">22222222</Button>
     </div>
   );
 }
